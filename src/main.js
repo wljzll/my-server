86aab8bb23b8446719eaec684ce3b52f7f7dfee5
zoulele <zoulele@xinye.com>
2022-03-20 21:14:13 +0800

    second commit

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ const chalk = require("chalk");
 const { createReadStream, readFileSync } = require("fs");
 const { render } = require("./util");
 const zlib = require("zlib");
+const crypto = require('crypto');
 
 // 获取本地电脑的IPv4地址
 let interfaces = os.networkInterfaces();
@@ -56,7 +57,7 @@ module.exports = class Server {
       // 访问的路径或者文件不存在进行错误处理
       this.sendError(res, error);
     }
-  };
+  }
   gzip(req, res) {
     // 在发送之前 进行压缩处理 浏览器和服务器说 Accept-Encoding: gzip, deflate, br(浏览器支持的压缩方式)
     // 服务器返回: content-encoding: gzip(服务器是通过gzip压缩的)
@@ -84,7 +85,41 @@ module.exports = class Server {
     }
     return zip;
   }
+  cache(req, res, statObj, filePath) {
+    // 强制缓存 - 
+    // 1. 强制缓存对于直接请求的资源不生效，比如我们直接请求index.html index.html是不会被缓存的
+    // 2. 浏览器不能开Disable Cache选项 不然也会不生效
+    // res.setHeader('Cache-Control', 'max-age=10'); //单位：S 10S内我引用的其他资源不要再访问了
+    // res.setHeader('Expires', new Date(Date.now() + 10 * 1000).toGMTString())
+
+
+    // 协商缓存
+    
+    // 1. 根据文件修改时间来协商
+    res.setHeader('Cache-Control', 'no-cache'); // no-cache表示浏览器缓存中有 但是每次也要来服务器询问  no-store: 表示不缓存
+    const IfModifiedSince = req.headers['if-modified-since'];
+    const ctime = statObj.ctime.toGMTString(); // 文件的最后修改时间
+    res.setHeader('last-modified', ctime);
+
+    // 根据文件的最后修改时间来判断 会有种情况就是文件修改时间变了，但是内容没变化 或者1S内多次改变也监控不到
+    if(IfModifiedSince !== ctime) { // 上次返回给客户端的文件修改时间和本次请求时的文件修改时间是否相同
+        return false;
+    }
+    
+    // 2. 根据内容来生成一个唯一的标识 ETAG来协商
+    const ifNoneMatch = req.headers['if-none-match'];
+    let etag = crypto.createHash('md5').update(readFileSync(filePath)).digest('base64'); // 实际上 不会去读取文件内容来生成摘要
+    res.setHeader('ETag', etag);
+    if(ifNoneMatch !== etag) { // 上次返回给客户端的文件内容摘要和本次请求时的文件内容摘要是否相同
+        return false;
+    }
+    return true;
+  }
   sendFile(req, res, statObj, filePath) {
+    if (this.cache(req, res, statObj, filePath)) {
+        res.statusCode = 304;
+        return res.end();
+    }
     const zip = this.gzip(req, res);
     // 如果HTML文件不指定编码　这里也不设置　会乱码
     // 如果文件没有后缀名的话 默认是文本
@@ -102,6 +137,7 @@ module.exports = class Server {
   }
   // 路径错误处理
   sendError(res, error) {
+      console.log(error);
     // 将状态码设置成404
     res.statusCode = 404;
     // 设置响应内容
